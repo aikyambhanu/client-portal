bdc76bcedc0a6bb57a4e62a2940794552ebf1d47
aikyambhanu <aikyamweb@gmail.com>
2026-04-18 21:47:57 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,4 @@
-'use client'
-
+import { supabase } from '../../lib/supabaseClient'
 import { useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import { useRouter } from 'next/navigation'
